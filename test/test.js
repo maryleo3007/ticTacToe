d3d9702acb4c5cb9f12ca04202e5b02423f756b4
Marilu Llamocca <maryleo.3007@gmail.com>
2017-03-06 10:33:17 -0500

    agregando test

---
+++ test/test.js
@@ -0,0 +1,16 @@
+var assert = require('assert');
+var app = require('../assest/js/app.js');
+describe("Tictactoe add test",function(){
+  it("Poder iniiciar un nuevo juego",function () {
+
+  });//primero probar que se ponga
+  it("Un jugador X debe poder hacer su jugada",function () {
+
+  });
+  it("Validar que algun jugador gano el juego",function () {
+
+  });
+  it("Validar que hay algun empate",function () {
+
+  });
+});
